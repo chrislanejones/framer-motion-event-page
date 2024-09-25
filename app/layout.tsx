0a945b66265dbdecf01f0b25ffd3a4f23102c2bf
Chris Lane Jones <chrislanejones@gmail.com>
2024-09-24 23:49:04 -0400

    Adding Content and Fixing Tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { SectionHeader } from "@/utils/SectionHeader";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SectionHeader title="About" dir="l" />
         {children}
       </body>
     </html>
