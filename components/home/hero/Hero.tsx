cb2dd83c4fe1cb717ad6c0b4b4bdb1cd00f78c03
Chris Lane Jones <chrislanejones@gmail.com>
2024-10-08 23:54:21 -0400

    Testing Out Grid Placement on Hero

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -8,11 +8,11 @@ import { StandardButton } from "@/components/buttons/StandardButton";
 export const Hero = () => {
   return (
     <section className={`section-wrapper ${"hero"}`}>
-      <div className="heroGrid flex flex-nowrap flex-row items-center flex-column justify-between gap-6">
-        <div className="relative z-10 w-fit my-[4.8rem]">
+      <div className="test">
+        <div className="test1">
           <Reveal>
             <h1 className="title text-8xl font-black">
-              Marketing Event
+              Event
               <span className="text-fuchsia-500">.</span>
             </h1>
           </Reveal>
@@ -22,12 +22,7 @@ export const Hero = () => {
             </h2>
           </Reveal>
           <Reveal>
-            <p className="max-w-80 px-0 py-10">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
+            <p className="max-w-80 px-0 py-10">Lorem ipsum dolor sit amet</p>
           </Reveal>
           <Reveal>
             <StandardButton
@@ -39,13 +34,16 @@ export const Hero = () => {
             </StandardButton>
           </Reveal>
         </div>
+        <div className="test2">
+          <DotGrid />
+        </div>
         <motion.div
           initial={{ y: -30, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.4 }}
         >
           <Image
-            className="relative z-10 m-8 mr-36 outline outline-3 outline-offset-6 transition duration-400"
+            className="test3"
             src={DataCenter}
             priority
             alt="John Carlo Devera | Frontend Developer"
@@ -54,7 +52,6 @@ export const Hero = () => {
           />
         </motion.div>
       </div>
-      <DotGrid />
     </section>
   );
 };
