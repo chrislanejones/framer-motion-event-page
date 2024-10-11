691103ee882c55632804165e293b75e7923f8bbd
Chris Lane Jones <chrislanejones@gmail.com>
2024-10-10 23:59:22 -0400

    Updated Heading Code

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -1,3 +1,17 @@
 export const Heading = () => {
-  return <header className="heading"></header>;
+  return (
+    <header
+      className="h-[calc(45px+3.6rem)] 
+px-[3.6rem]
+flex items-center justify-between
+sticky top-0 z-20
+bg-[var(--bg-opaque)]
+backdrop-filter blur-[12px]
+text-[var(--text-md)] font-bold
+@media (max-width: 768px) {
+  px-[2.4rem]
+}
+"
+    ></header>
+  );
 };
