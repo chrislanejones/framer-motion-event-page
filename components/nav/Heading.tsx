3f30097201a38e8c608b7fa0dc7f815853cd5d42
Chris Lane Jones <chrislanejones@gmail.com>
2024-10-11 23:22:18 -0400

    Fixing the Grid on the Sidebar

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -1,17 +1,7 @@
 export const Heading = () => {
   return (
-    <header
-      className="h-[calc(45px+3.6rem)] 
-px-[3.6rem]
-flex items-center justify-between
-sticky top-0 z-20
-bg-[var(--bg-opaque)]
-backdrop-filter blur-[12px]
-text-[var(--text-md)] font-bold
-@media (max-width: 768px) {
-  px-[2.4rem]
-}
-"
-    ></header>
+    <header className="h-[calc(45px+3.6rem)] px-[3.6rem] flex items-center justify-between sticky top-0 z-20 bg-[var(--bg-opaque)] backdrop-filter backdrop-blur-[12px] text-[var(--text-md)] font-bold md:px-[2.4rem]">
+      March 18, 2025
+    </header>
   );
 };
--- components/nav/Sidebar.tsx
+++ components/nav/Sidebar.tsx
@@ -26,65 +26,72 @@ export const SideBar = () => {
   }, []);
 
   return (
-    <motion.nav
-      initial={{ x: -70 }}
-      animate={{ x: 0 }}
-      transition={{ duration: 0.5 }}
-      className="sidebar bg-[var(--background-dark)] h-screen sticky top-0 left-0 z-20 flex flex-col items-center"
-    >
-      <span
-        className="[writing-mode:vertical-lr] cursor-pointer flex-shrink-0 text-md font-bold line-height-1 w-45 h-45 flex items-center justify-center bg-background rounded-md mx-auto my-6"
-        onClick={() => {
-          document.location.hash === ""
-            ? document.getElementById("main")?.scrollIntoView()
-            : (document.location.hash = "");
-        }}
-      >
-        UX Now
-        <span className="text-md font-bold line-height-1 primary-color">.</span>
-      </span>
-      <motion.a
+    <div className="[writing-mode:vertical-lr] h-screen sticky grid grid-cols-6 justify-evenly content-evenly gap-2  top-0 left-0 z-20">
+      <motion.nav
         initial={{ x: -70 }}
         animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-        href="#Time"
-        onClick={() => {
-          setSelected("Time");
-        }}
-        className={selected === "Time" ? "selected" : ""}
+        transition={{ duration: 0.5 }}
+        className="sidebar bg-[var(--background-dark)]   text-lg m-4"
       >
-        Time & Date
-      </motion.a>
-      <motion.a
-        initial={{ x: -70 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-        href="#projects"
-        onClick={() => setSelected("projects")}
-        className={selected === "Speakers" ? selected : ""}
-      >
-        Speakers
-      </motion.a>
-      <motion.a
-        initial={{ x: -70 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.3 }}
-        href="#Info"
-        onClick={() => setSelected("Info")}
-        className={selected === "Info" ? selected : ""}
-      >
-        More Info
-      </motion.a>
-      <motion.a
-        initial={{ x: -70 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-        href="#contact"
-        onClick={() => setSelected("contact")}
-        className={selected === "contact" ? selected : ""}
-      >
-        Contact
-      </motion.a>
-    </motion.nav>
+        <span
+          className="[writing-mode:vertical-lr] cursor-pointer text-3xl line-height-1 w-45 h-45  bg-background rounded-md mx-auto my-6"
+          onClick={() => {
+            if (document.location.pathname !== "/") {
+              // Navigate to the home page
+              document.location.pathname = "/";
+            } else {
+              // Scroll to the top of the page
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }
+          }}
+        >
+          UX Now
+          <span className=" font-bold line-height-1 primary-color">.</span>
+        </span>
+
+        <motion.a
+          initial={{ x: -70 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          href="#Time"
+          onClick={() => {
+            setSelected("Time");
+          }}
+          className={selected === "Time" ? "selected" : ""}
+        >
+          Time & Date
+        </motion.a>
+        <motion.a
+          initial={{ x: -70 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          href="#projects"
+          onClick={() => setSelected("projects")}
+          className={selected === "Speakers" ? selected : ""}
+        >
+          Speakers
+        </motion.a>
+        <motion.a
+          initial={{ x: -70 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          href="#Info"
+          onClick={() => setSelected("Info")}
+          className={selected === "Info" ? selected : ""}
+        >
+          More Info
+        </motion.a>
+        <motion.a
+          initial={{ x: -70 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          href="#contact"
+          onClick={() => setSelected("contact")}
+          className={selected === "contact" ? selected : ""}
+        >
+          Contact
+        </motion.a>
+      </motion.nav>
+    </div>
   );
 };
